94bbcdde78c1725abfb1312dca90ce2cf47e4c9d
kullop <burakcankarasel@gmail.com>
2022-08-03 23:59:19 +0300

    Added enviroment variables to our code

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,17 +6,18 @@ import (
 
 	"github.com/burakkarasel/Bank-App/api"
 	db "github.com/burakkarasel/Bank-App/db/sqlc"
+	"github.com/burakkarasel/Bank-App/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:password@localhost:5432/bank_app?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".") // we pass the location of the file
+
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +26,7 @@ func main() {
 
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server:", err)
