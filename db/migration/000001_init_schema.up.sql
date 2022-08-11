6ce97dc090dbcf80f2c46422aa26a9905f3c3256
kullop <burakcankarasel@gmail.com>
2022-08-11 19:38:10 +0300

    Added new queries for sessions in DB, updated mock DB queries, updated migrations

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -30,6 +30,19 @@ CREATE TABLE "users" (
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
+CREATE TABLE "sessions" (
+  "id" uuid PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "refresh_token" varchar NOT NULL,
+  "user_agent" varchar NOT NULL,
+  "client_ip" varchar NOT NULL,
+  "is_blocked" boolean NOT NULL DEFAULT false,
+  "expires_at" timestamptz NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "sessions" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
+
 CREATE INDEX ON "accounts" ("owner");
 
 --CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
