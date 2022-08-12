64d254c098b2bc4893df7014a8859a2893f08186
kullop <burakcankarasel@gmail.com>
2022-08-12 19:41:34 +0300

    Added new gateway and gRPC server runners, implemented db migrations directly into code

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/burakkarasel/Bank-App/api"
 	db "github.com/burakkarasel/Bank-App/db/sqlc"
 	"github.com/burakkarasel/Bank-App/gapi"
 	"github.com/burakkarasel/Bank-App/pb"
 	"github.com/burakkarasel/Bank-App/util"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -28,10 +35,14 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 
+	// run db migrations here
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 
-	runGrpcServer(config, store)
-	// runGinServer(config, store)
+	/* go runGatewayServer(config, store)
+	runGrpcServer(config, store) */
+	runGinServer(config, store)
 }
 
 // runGinServer runs a gin HTTP server
@@ -55,17 +66,18 @@ func runGinServer(config util.Config, store db.Store) {
 func runGrpcServer(config util.Config, store db.Store) {
 	server, err := gapi.NewServer(config, store)
 	if err != nil {
-		log.Fatal("cannot create server")
+		log.Fatal("cannot create server", err)
 	}
 
 	grpcServer := grpc.NewServer()
 
 	pb.RegisterBankAppServer(grpcServer, server)
-	reflection.Register(grpcServer) // self doc
+	// this command enables CLI to see which rpc's are avaiable and how can we call them
+	reflection.Register(grpcServer)
 
 	listener, err := net.Listen("tcp", config.GrpcServerAddress)
 	if err != nil {
-		log.Fatal("cannot create listener")
+		log.Fatal("cannot create listener:", err)
 	}
 
 	log.Printf("gRPC server started at %s", listener.Addr().String())
@@ -73,6 +85,69 @@ func runGrpcServer(config util.Config, store db.Store) {
 	err = grpcServer.Serve(listener)
 
 	if err != nil {
-		log.Fatal("cannot start server")
+		log.Fatal("cannot start server", err)
 	}
+}
+
+// runGatewayServer runs a HTTP server which enables both gRPC and HTTP requests
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+
+	// to use snake case instead of camel case in requests and responses for the consistency
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterBankAppHandlerServer(ctx, grpcMux, server)
+
+	if err != nil {
+		log.Fatal("cannot register handler server", err)
+	}
+
+	// here we register our grpc routes to http routes
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	fs := http.FileServer(http.Dir("./doc/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener", err)
+	}
+
+	log.Printf("HTTP server started at %s", listener.Addr().String())
+
+	err = http.Serve(listener, mux)
+
+	if err != nil {
+		log.Fatal("cannot start HTTP gateway server", err)
+	}
+}
+
+// runDBMigration runs the migrations at the start of the program
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance:", err)
+	}
+
+	if err := migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up:", err)
+	}
+
+	log.Println("db migrated succesfully")
 }
